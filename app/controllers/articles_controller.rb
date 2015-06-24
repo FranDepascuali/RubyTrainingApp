0ab2aa701f0b82127cc55c41f40fc4dbe8919e88
Francisco Depascuali <francisco.depascuali@wolox.com.ar>
2015-06-24 09:34:29 -0300

    style fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,23 @@
 class ArticlesController < ApplicationController
-	def index
+  def index
     @articles = Article.all
   end
- 
+
   def show
     @article = Article.find(params[:id])
   end
 
-	def new
-		@article = Article.new
-	end
+  def new
+    @article = Article.new
+  end
 
-	def create
-		@article = Article.new(article_params)
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
-	end
+  def create
+    return redirect_to @article if @article.save
+    render 'new'
+  end
 
-	private
-		def article_params
-			params.require(:article).permit(:title, :text)
-		end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
